7c7500509d0a6be5ae17b5970c6ecafbb9b40eed
Ayrat Badykov <ayratin555@gmail.com>
2025-02-11 13:28:45 +0200

    draft derivation logic

--- native/guarana/src/lib.rs
+++ native/guarana/src/lib.rs
@@ -4,7 +4,6 @@ use rustler::Encoder;
 use rustler::Env;
 use rustler::ListIterator;
 use rustler::NewBinary;
-use rustler::NifResult;
 use rustler::Term;
 
 mod atoms {
@@ -17,29 +16,57 @@ mod atoms {
     }
 }
 
-fn derive_key(
+rustler::init!("Elixir.Guarana.Impl");
+
+#[rustler::nif]
+fn derive_key<'a>(
     env: Env<'a>,
     secret_key: Binary,
     chain_code: Binary,
     path_list: ListIterator<'a>,
 ) -> Term<'a> {
-    let extended_key = load_extended_key(env, secret_key, chain_code)?;
+    let extended_key = match load_extended_key(env, secret_key, chain_code) {
+        Ok(result) => result,
+        Err(error) => return error,
+    };
 
     let path = path_list
-        .map(|x| match x.decode::<i64>() {
-            Ok(number) => number,
-            Err(_) => return Err((atoms::error(), atoms::invalid_path()).encode(env)),
-        })
-        .collect::<NifResult<Vec<i64>>>();
+        .map(|x| x.decode::<u32>().unwrap())
+        .collect::<Vec<u32>>();
 
     let child_key_pair = Edwards::derive_child_key_pair_with_path(&extended_key, path);
 
+    let mut public_key = NewBinary::new(env, 32);
+    public_key
+        .as_mut_slice()
+        .copy_from_slice(&child_key_pair.public_key().public_key.to_bytes(false)[..]);
+
+    let mut public_key_chain_code = NewBinary::new(env, 32);
+    public_key_chain_code
+        .as_mut_slice()
+        .copy_from_slice(&child_key_pair.public_key().chain_code);
+
+    let mut private_key = NewBinary::new(env, 64);
+    private_key.as_mut_slice().copy_from_slice(
+        &child_key_pair
+            .secret_key()
+            .secret_key
+            .as_ref()
+            .to_be_bytes()[..],
+    );
+
+    let mut private_key_chain_code = NewBinary::new(env, 32);
+    private_key_chain_code
+        .as_mut_slice()
+        .copy_from_slice(&child_key_pair.secret_key().chain_code);
+
     (
-        Binary::from(child_key_pair.secret_key().secret_key),
-        child_key_pair.secret_key().chain_code,
-        Binary::from(child_key_pair.public_key().public_key),
-        child_key_pair.public_key().chain_code,
+        Binary::from(public_key),
+        Binary::from(private_key_chain_code),
+        Binary::from(private_key),
+        Binary::from(public_key_chain_code),
     )
+        .encode(env)
 }
 
 fn load_extended_key<'a>(
