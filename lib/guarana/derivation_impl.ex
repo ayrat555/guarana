418acbc1fb65acd0a2ca693cfde70088dac6e880
Ayrat Badykov <ayratin555@gmail.com>
2025-02-12 15:11:50 +0200

    typo

--- lib/guarana/derivation_impl.ex
+++ lib/guarana/derivation_impl.ex
@@ -7,7 +7,7 @@ defmodule Guarana.DerivationImpl do
     otp_app: :guarana,
     crate: :guarana,
     base_url: "https://github.com/ayrat555/guarana/releases/download/v#{version}",
-    force_build: System.get_env("RUSTLER_BUILD") in ["1", "true"],,
+    force_build: System.get_env("RUSTLER_BUILD") in ["1", "true"],
     targets: Enum.uniq(["x86_64-unknown-freebsd" | RustlerPrecompiled.Config.default_targets()]),
     nif_versions: ["2.15", "2.16"],
     version: version
